442c519002aa8ef1be89e6bedf61976be0d03cc7
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-02 18:21:51 +0530

    Add pseudo code for getting a random number
    Added the pseudo code for getting the random number based on dividing
    the value 1 into no of random numbers needed equal parts. Now by
    checking if the generated random number using Math.random() method lies
    in which part of the equal divided parts we return the desired random
    number.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+// Function to get random number
+    // Get a random number between 0 and 1
+    // Divide 1 by the number of random numbers needed, the result value divides the number 1 into the required random numbers equal parts 
+    // Check if the random number is lower than the first part, if true return one of the desired random number
+    // Check if the random number is lower than the second part, if true return some other random number
+    // Repeat it until only the last part is needed to be checked in which case return the last remaining desired random number
