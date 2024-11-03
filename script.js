6857c8b0051f08ea07482fe0a6f9d45e228f1356
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-03 12:46:50 +0530

    Move game logic and variables to new playGame func
    Added a new playGame function and move the round's gameplay and
    game's scores, choices and round tracking variables declaration to it
    so that everything related to the game is present inside it.

--- script.js
+++ script.js
@@ -16,163 +16,175 @@ function getRandomNumber(noOfRandomNumbers, startsFrom) {
 }
 
 
-// Function to get computer choice
-function getComputerChoice() {
-    // Get a random integer number between 1 to 3
-    const randomNumber = getRandomNumber(3, 1);
-
-    // If random number is 1 return "rock"
-    if(randomNumber === 1) {
-        return "rock"
-    }
-
-    // If random number is 2 return "paper"
-    else if(randomNumber === 2) {
-        return "paper"
-    }
-
-    // If random number is 3 return "scissors"
-    else {
-        return "scissors"
-    }
-}
-
-
-// Function to get user's choice
-function getHumanChoice() {
-    // Ask the user for his choice and store his choice in a variable, also set empty string as default value when no input provided
-    const userChoice = prompt("Enter your choice below") || "";
-
-    // Convert the user choice to lowercase and return it
-    return userChoice.toLowerCase();
-}
-
-
-let humanScore = 0;
-let computerScore = 0;
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-console.log("User Choice: ", humanChoice);
-console.log("Computer Choice: ", computerChoice);
-
-
-// Function to get the result of the round
-function getResult(humanChoice, computerChoice) {
-
-    // Check if user and computer choices are the same, if same then return "tie" as result
-    if(humanChoice === computerChoice) {
-        return "tie";
-    }
-
-    // Winner deciding switch case logic for the round based on both players choices
-    switch(humanChoice) {
-        case "rock":
-            // If user chose "rock" and computer chose "scissors" then return "win"
-            if(computerChoice === "scissors") {
-                return "win";
-            }
-            // If user chose "rock" and computer chose "paper" then return "lose"
-            else if(computerChoice === "paper") {
-                return "lose";
-            }
-            else {
-                return "Invalid choice";
-            }
-
-        case "paper":
-            // If user chose "paper" and computer chose "rock" then return "win"
-            if(computerChoice === "rock") {
-                return "win";
-            }
-            // If user chose "paper" and computer chose "scissors" then return "lose"
-            else if(computerChoice === "scissors") {
-                return "lose";
-            }
-            // Else return "invalid choice"
-            else {
-                return "Invalid choice";
-            }
-
-        case "scissors":
-            // If user chose "scissors" and computer chose "paper" then return "win"
-            if(computerChoice === "paper") {
-                return "win";
-            }
-            // If user chose "scissors" and computer chose "rock" then return "lose"
-            else if(computerChoice === "rock") {
-                return "lose";
-            }
-            // Else return "invalid choice"
-            else {
-                return "Invalid choice";
-            }
-
-        default:
-            // Else return "invalid choice"
-            return "Invalid choice";
-    }
-}
-
-
 // Function to capitalise text
 function capitalise(text) {
     return text.at(0).toUpperCase() + text.slice(1);
 }
 
 
-// Function to show result statement for the round
-function showResult(result, humanChoice, computerChoice) {
-    // If result is "tie" print tie statement
-    if(result === "tie") {
-        console.log(`It's a tie! You both chose ${capitalise(humanChoice)}`);
+// Function to start playing the game
+function playGame() {
+
+    // Declare and initialise choice, score and round tracking variables
+    let humanChoice = null;
+    let computerChoice = null;
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 1;
+
+
+    // Function to get computer choice
+    function getComputerChoice() {
+        // Get a random integer number between 1 to 3
+        const randomNumber = getRandomNumber(3, 1);
+
+        // If random number is 1 return "rock"
+        if(randomNumber === 1) {
+            return "rock"
+        }
+
+        // If random number is 2 return "paper"
+        else if(randomNumber === 2) {
+            return "paper"
+        }
+
+        // If random number is 3 return "scissors"
+        else {
+            return "scissors"
+        }
     }
-    // If result is "win" print win statement
-    else if(result === "win") {
-        console.log(`You won! ${capitalise(humanChoice)} beats ${capitalise(computerChoice)}`);
+
+
+    // Function to get user's choice
+    function getHumanChoice() {
+        // Ask the user for his choice and store his choice in a variable, also set empty string as default value when no input provided
+        const userChoice = prompt("Enter your choice below") || "";
+
+        // Convert the user choice to lowercase and return it
+        return userChoice.toLowerCase();
     }
-    // If result is "lose" print lose statement
-    else if(result === "lose") {
-        console.log(`You lose! ${capitalise(computerChoice)} beats ${capitalise(humanChoice)}`);
+
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    console.log("User Choice: ", humanChoice);
+    console.log("Computer Choice: ", computerChoice);
+
+
+    // Function to get the result of the round
+    function getResult(humanChoice, computerChoice) {
+
+        // Check if user and computer choices are the same, if same then return "tie" as result
+        if(humanChoice === computerChoice) {
+            return "tie";
+        }
+
+        // Winner deciding switch case logic for the round based on both players choices
+        switch(humanChoice) {
+            case "rock":
+                // If user chose "rock" and computer chose "scissors" then return "win"
+                if(computerChoice === "scissors") {
+                    return "win";
+                }
+                // If user chose "rock" and computer chose "paper" then return "lose"
+                else if(computerChoice === "paper") {
+                    return "lose";
+                }
+                else {
+                    return "Invalid choice";
+                }
+
+            case "paper":
+                // If user chose "paper" and computer chose "rock" then return "win"
+                if(computerChoice === "rock") {
+                    return "win";
+                }
+                // If user chose "paper" and computer chose "scissors" then return "lose"
+                else if(computerChoice === "scissors") {
+                    return "lose";
+                }
+                // Else return "invalid choice"
+                else {
+                    return "Invalid choice";
+                }
+
+            case "scissors":
+                // If user chose "scissors" and computer chose "paper" then return "win"
+                if(computerChoice === "paper") {
+                    return "win";
+                }
+                // If user chose "scissors" and computer chose "rock" then return "lose"
+                else if(computerChoice === "rock") {
+                    return "lose";
+                }
+                // Else return "invalid choice"
+                else {
+                    return "Invalid choice";
+                }
+
+            default:
+                // Else return "invalid choice"
+                return "Invalid choice";
+        }
     }
-    // Else print invalid choice
-    else {
-        console.log("Invalid choice");
+
+
+    // Function to show result statement for the round
+    function showResult(result, humanChoice, computerChoice) {
+        // If result is "tie" print tie statement
+        if(result === "tie") {
+            console.log(`It's a tie! You both chose ${capitalise(humanChoice)}`);
+        }
+        // If result is "win" print win statement
+        else if(result === "win") {
+            console.log(`You won! ${capitalise(humanChoice)} beats ${capitalise(computerChoice)}`);
+        }
+        // If result is "lose" print lose statement
+        else if(result === "lose") {
+            console.log(`You lose! ${capitalise(computerChoice)} beats ${capitalise(humanChoice)}`);
+        }
+        // Else print invalid choice
+        else {
+            console.log("Invalid choice");
+        }
+    }
+
+
+    // Function to update score after the round
+    function updateScore(result) {
+        // If result is "win" increment user's score by 1
+        if(result === "win") {
+            humanScore = humanScore + 1;
+        }
+        // If result is "lose" increment computer's score by 1
+        else if(result === "lose") {
+            computerScore = computerScore + 1;
+        }
+        console.log(humanScore, "||" ,computerScore);
+    }
+
+
+    // Function to play the round
+    function playRound(humanChoice, computerChoice) {
+        
+        // Get the result for the round
+        const result = getResult(humanChoice, computerChoice);
+        console.log(result);
+
+        // Print the result statement for the round
+        showResult(result, humanChoice, computerChoice);
+
+        // Update score based on result
+        updateScore(result);
+    }
+
+
+    // If the user and computer choices are present then play the round
+    if(humanChoice && computerChoice) {
+        playRound(humanChoice, computerChoice);
     }
 }
 
 
-// Function to update score after the round
-function updateScore(result) {
-    // If result is "win" increment user's score by 1
-    if(result === "win") {
-        humanScore = humanScore + 1;
-    }
-    // If result is "lose" increment computer's score by 1
-    else if(result === "lose") {
-        computerScore = computerScore + 1;
-    }
-    console.log(humanScore, "||" ,computerScore);
-}
-
-
-// Function to play the round
-function playRound(humanChoice, computerChoice) {
-    
-    // Get the result for the round
-    const result = getResult(humanChoice, computerChoice);
-    console.log(result);
-
-    // Print the result statement for the round
-    showResult(result, humanChoice, computerChoice);
-
-    // Update score based on result
-    updateScore(result);
-}
-
-
-// If the user and computer choices are present then play the round
-if(humanChoice && computerChoice) {
-    playRound(humanChoice, computerChoice);
-}
+playGame();
